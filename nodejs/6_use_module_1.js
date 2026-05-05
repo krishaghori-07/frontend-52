b9723374bd502dd89dfe04cb530eabf34835d63c
theeasylearn <theeasylearn@gmail.com>
2026-05-05 12:06:23 +0530

    modules

---
+++ nodejs/6_use_module_1.js
@@ -0,0 +1,6 @@
+//import local module 
+// here dt is alias for datetime (we must use it call functions)
+const dt = require('./datetime');
+
+console.log(dt.getDate());
+console.log(dt.getTime()); 
