2cda2a26ff687552304e86c482d14ad2fba03924
theeasylearn <theeasylearn@gmail.com>
2026-06-17 12:50:44 +0530

    category api

--- nodejs/database/category_api.js
+++ nodejs/database/category_api.js
@@ -14,21 +14,20 @@ app.get(CATEGORY, function (request, response) {
 
 app.post(CATEGORY, function (request, response) {
 
-    var name = request.body.name;
-    var detail = request.body.detail;
+    const name = request.body.name;
+    const detail = request.body.detail;
     //check name and details are not empty 
 
     if (name === undefined || detail === undefined) {
-        response.send([{ 'error': 'input missing' }])
+        response.json([{ 'error': 'input missing' }])
     }
-    else 
-    {
+    else {
         //parameterized queries (also known as prepared statements)
         const sql = `insert into category (name,detail,photo) values (?,?,?)`;
         //create list that has 3 items because there 3 question marks(place holder)
-        const data = [name,detail,'photo.jpg']
+        const data = [name, detail, 'photo.jpg']
         // run sql statement
-        connection.con.query(sql,data,function (error, result) {
+        connection.con.query(sql, data, function (error, result) {
             //this function will run after sql statement executes if sql statement fails then error variable will be not null 
             // if sql statement success then result variable be not null 
             if (error != null) {
@@ -47,11 +46,30 @@ app.post(CATEGORY, function (request, response) {
 app.put(CATEGORY, function (request, response) {
     response.send('update request received');
 });
-
+//curl -X DELETE http://localhost:5000/category -H "Content-Type: application/x-www-form-urlencoded" -d "categoryid=123"
 app.delete(CATEGORY, function (request, response) {
-    response.send('delete request received');
+    const categoryid = request.body.categoryid;
+    if (categoryid === undefined) {
+        response.json([{ 'error': 'input missing' }])
+    }
+    else {
+        // ? is called placeholder/parameter
+        const sql = "delete from category where id=?";
+        const data = [categoryid];
+        connection.con.query(sql,data,function (error, result) {
+            if (error != null) {
+                console.log(error)
+                response.status(500).json([{ 'error': 'Internal server error' }]);
+            }
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'category deleted' }])
+            }
+        });
+    }
+
+
 });
 
 app.listen(5000);
-console.log('ready to accept reqeust')
+console.log('ready to accept request')
 
