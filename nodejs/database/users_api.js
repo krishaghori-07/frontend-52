84a6d49a7bbd343cd583f7af94b08a1eb3bb1e9d
theeasylearn <theeasylearn@gmail.com>
2026-06-24 10:53:26 +0530

    USERS

--- nodejs/database/users_api.js
+++ nodejs/database/users_api.js
@@ -1,8 +1,11 @@
 //database connection import
 const connection = require('./connection')
+const security = require('./security');
+
 //import express 
 const express = require('express');
 var bodyParser = require('body-parser');
+
 
 var app = express();
 
@@ -19,24 +22,66 @@ app.post(USERS + "/register", function (request, response) {
     let mobile = request.body.mobile;
     let password = request.body.password;
 
+    if (email === undefined || mobile === undefined || password === undefined) {
+        response.json([{ 'error': 'input missing' }]);
+    }
     sql = "insert into users (email,mobile,password) values (?,?,?)";
-    values = [email,mobile,password];
-    connection.con.query(sql,values, function (error, result) {
-        if (error != null) {
-            console.log(error);
-             response.status(500).json([{ 'error': 'oops something went wrong' }])
-        }
-        else {
-            // no error
-            response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'registered successfully' }])
-        }
+    // password hash
+    let pwd = new security.PasswordManager();
+    pwd.getHashPassword(password).then((HashedPassword) => {
+
+        values = [email, mobile, HashedPassword];
+
+        connection.con.query(sql, values, function (error, result) {
+            if (error != null) {
+                console.log(error);
+                if (error.errno === 1062) {
+                    response.json([{ 'error': 'either email and/or mobile is already registered with us, please use different email/mobile ' }])
+                }
+                else {
+                    response.status(500).json([{ 'error': 'oops something went wrong' }])
+                }
+            }
+            else {
+                // no error
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'registered successfully' }])
+            }
+        });
     });
+
 });
 
 //create route for login 
 // input : email, password, 
 app.post(USERS + "/login", function (request, response) {
-    response.send("post request received");
+    var sql = "select id,password from users where email=?";
+    let data = ['krisha@gmail.com']
+    connection.con.query(sql, data, function (error, result, fields) {
+        if (error != null) {
+            console.log(error);
+            response.status(500).json([{ 'error': 'oops something went wrong' }])
+        }
+
+        else {
+            //fetch password from database
+            let HashedPassword = result[0]['password'];
+            let PlainTextPassword = 'apple';
+            console.log(HashedPassword);
+            let pwd = new security.PasswordManager();
+            pwd.comparePassword(PlainTextPassword, HashedPassword).then((isMatched) => {
+                if (isMatched == false) {
+                    response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'login successful' }])
+
+                }
+                else {
+                    response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'failed login attempt' }])
+
+                }
+            })
+        }
+
+    });
+
 });
 
 //create route for change password
