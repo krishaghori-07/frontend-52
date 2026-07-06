d0e029efa2e608a93dcd57da29edb8ac6196f9a8
theeasylearn <theeasylearn@gmail.com>
2026-07-06 11:36:57 +0530

    day 3

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,10 @@
+// import React Module
 import React from 'react';
+// import virtual DOM
 import ReactDOM from 'react-dom/client';
+// import css file from src (optional)
+// always use ./ while importing files from src folder
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<h1>Hello, we have just started using React JS. <br/> Happy Coding</h1>);
