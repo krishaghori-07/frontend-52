a4b2157698f20b6bd69f46d25f6df1f58e9949cb
theeasylearn <theeasylearn@gmail.com>
2026-08-03 11:43:55 +0530

    calculator

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,270 +1,156 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-class Company extends React.Component {
-    //create constructor
-    constructor(props) {
-        super(props); //required 
-        //create property variable
-        this.name = props.name;
-        this.sector = props.sector;
-        this.year = props.year;
-        this.marketCap = props.marketCap;
-        this.srno = props.no;
-    }
-
-    deleteCompany = () => {
-        //alert(this.name + "will be deleted");
-        this.props.deleteCompany(this.name);
-    }
-    render() {
-        return (<tr>
-            <td>{this.srno}</td>
-            <td>{this.name}</td>
-            <td>{this.sector}</td>
-            <td>{this.year}</td>
-            <td>{this.marketCap}</td>
-            <td>
-                <button onClick={this.deleteCompany} type='button' className='btn btn-danger'>Delete</button></td>
-        </tr>)
-    }
-}
-class BSE extends React.Component {
+import './calculator.css';
+class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        //state object
+        //create empty state object
         this.state = {
-            sensexCompanies: [
-                {
-                    company: "Adani Ports & Special Economic Zone",
-                    established: 1998,
-                    sector: "Infrastructure & Ports",
-                    marketCapCrore: 320000
-                },
-                {
-                    company: "Asian Paints",
-                    established: 1942,
-                    sector: "Paints / Consumer",
-                    marketCapCrore: 230000
-                },
-                {
-                    company: "Axis Bank",
-                    established: 1993,
-                    sector: "Private Banking",
-                    marketCapCrore: 430000
-                },
-                {
-                    company: "Bajaj Finance",
-                    established: 1987,
-                    sector: "NBFC",
-                    marketCapCrore: 580000
-                },
-                {
-                    company: "Bajaj Finserv",
-                    established: 2007,
-                    sector: "Financial Services",
-                    marketCapCrore: 350000
-                },
-                {
-                    company: "Bharti Airtel",
-                    established: 1995,
-                    sector: "Telecommunications",
-                    marketCapCrore: 1106589
-                },
-                {
-                    company: "Eternal (formerly Zomato)",
-                    established: 2008,
-                    sector: "Internet / Food Delivery",
-                    marketCapCrore: 280000
-                },
-                {
-                    company: "HCLTech",
-                    established: 1991,
-                    sector: "Information Technology",
-                    marketCapCrore: 440000
-                },
-                {
-                    company: "HDFC Bank",
-                    established: 1994,
-                    sector: "Private Banking",
-                    marketCapCrore: 1137193
-                },
-                {
-                    company: "Hindustan Unilever",
-                    established: 1933,
-                    sector: "FMCG",
-                    marketCapCrore: 560000
-                },
-                {
-                    company: "ICICI Bank",
-                    established: 1994,
-                    sector: "Private Banking",
-                    marketCapCrore: 1020000
-                },
-                {
-                    company: "IndusInd Bank",
-                    established: 1994,
-                    sector: "Private Banking",
-                    marketCapCrore: 70000
-                },
-                {
-                    company: "Infosys",
-                    established: 1981,
-                    sector: "Information Technology",
-                    marketCapCrore: 670000
-                },
-                {
-                    company: "ITC",
-                    established: 1910,
-                    sector: "FMCG",
-                    marketCapCrore: 530000
-                },
-                {
-                    company: "Kotak Mahindra Bank",
-                    established: 1985,
-                    sector: "Private Banking",
-                    marketCapCrore: 420000
-                },
-                {
-                    company: "Larsen & Toubro",
-                    established: 1938,
-                    sector: "Engineering & Construction",
-                    marketCapCrore: 520000
-                },
-                {
-                    company: "Mahindra & Mahindra",
-                    established: 1945,
-                    sector: "Automobile",
-                    marketCapCrore: 460000
-                },
-                {
-                    company: "Maruti Suzuki India",
-                    established: 1981,
-                    sector: "Automobile",
-                    marketCapCrore: 390000
-                },
-                {
-                    company: "Nestlé India",
-                    established: 1959,
-                    sector: "FMCG",
-                    marketCapCrore: 225000
-                },
-                {
-                    company: "NTPC",
-                    established: 1975,
-                    sector: "Power Generation",
-                    marketCapCrore: 350000
-                },
-                {
-                    company: "Power Grid Corporation of India",
-                    established: 1989,
-                    sector: "Power Transmission",
-                    marketCapCrore: 280000
-                },
-                {
-                    company: "Reliance Industries",
-                    established: 1973,
-                    sector: "Oil, Gas & Conglomerate",
-                    marketCapCrore: 1710175
-                },
-                {
-                    company: "State Bank of India",
-                    established: 1955,
-                    sector: "Public Sector Banking",
-                    marketCapCrore: 720000
-                },
-                {
-                    company: "Sun Pharmaceutical Industries",
-                    established: 1983,
-                    sector: "Pharmaceuticals",
-                    marketCapCrore: 410000
-                },
-                {
-                    company: "Tata Consultancy Services",
-                    established: 1968,
-                    sector: "Information Technology",
-                    marketCapCrore: 1190000
-                },
-                {
-                    company: "Tata Motors",
-                    established: 1945,
-                    sector: "Automobile",
-                    marketCapCrore: 250000
-                },
-                {
-                    company: "Tata Steel",
-                    established: 1907,
-                    sector: "Steel",
-                    marketCapCrore: 210000
-                },
-                {
-                    company: "Tech Mahindra",
-                    established: 1986,
-                    sector: "Information Technology",
-                    marketCapCrore: 170000
-                },
-                {
-                    company: "Titan Company",
-                    established: 1984,
-                    sector: "Consumer Goods / Jewellery",
-                    marketCapCrore: 310000
-                },
-                {
-                    company: "UltraTech Cement",
-                    established: 1983,
-                    sector: "Cement",
-                    marketCapCrore: 360000
-                }
-            ]
+            answer : ''
         };
     }
 
-    deleteCompany = (name) => {
-        alert("parent delete company method called." + name);
-        var temp = this.state.sensexCompanies.filter((item) => {
-            //console.log(item.company,name);
-            if (item.company !== name)
-                return item
-        });
-        console.log(temp.length);
-        
+    //arrow function (event is automatically received argument (implicit argument))
+    calculateResult = (event) => {
+        //display state variable
+        console.log(this.state);
+        event.preventDefault(); //compulsory otherwise page will refresh
+        let result = null;
+        if(this.state.operation === '1')
+        {
+            result = this.state.num1 + this.state.num2
+        }
+        else if(this.state.operation === '2')
+        {
+            result = this.state.num1 - this.state.num2
+        }
+        else if(this.state.operation === '3')
+        {
+            result = this.state.num1 * this.state.num2
+        }
+        else if(this.state.operation === '4')
+        {
+            result = this.state.num1 / this.state.num2
+        }
         this.setState({
-            sensexCompanies: temp
-        });
+            answer: result
+        }); //this will call render method 
+    }
+
+    //arrow function (event is automatically received argument (implicit argument))
+    updateInput = (event) => {
+        //create or update state variable
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
     render() {
         return (<div className="container">
             <div className="row">
                 <div className="col-12">
                     <div className="card">
-                        <div className="card-header">
-                            <h1>BSE 30 companies </h1>
+                        <div className="card-header text-center">
+                            <h4>Simple Calculator</h4>
                         </div>
                         <div className="card-body">
-                            <table className="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Sr No</th>
-                                        <th>Company Name</th>
-                                        <th>Sector</th>
-                                        <th>Establishment Year</th>
-                                        <th>Market cap</th>
-                                        <th>Delete</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.state.sensexCompanies.map((item, index) => {
-                                        return <Company no={index + 1} name={item.company} sector={item.sector} year={item.established} marketCap={item.marketCapCrore} key={index}
-                                            deleteCompany={this.deleteCompany} />
-                                    })}
-                                </tbody>
-                            </table>
+                            <form action="#" method="POST"
+                                onSubmit={this.calculateResult}
+                            >
+                                {/* First Number */}
+                                <div className="mb-4">
+                                    <label htmlFor="num1" className="form-label">First Number</label>
+                                    <input type="number" className="form-control" id="num1" name="num1"
+                                        value={this.state.num1}
+                                        onChange={(e) => this.updateInput(e)}
+                                        placeholder="Enter first number"
+                                        step="any" required />
+                                </div>
+                                {/* Second Number */}
+                                <div className="mb-4">
+                                    <label htmlFor="num2" className="form-label">Second Number</label>
+                                    <input type="number" className="form-control" id="num2" name="num2"
+                                        value={this.state.num2}
+                                        onChange={(e) => this.updateInput(e)}
+                                        placeholder="Enter second number"
+                                        step="any" required />
+                                </div>
+                                {/* Operation */}
+                                <div className="mb-4">
+                                    <label className="form-label d-block">Operation</label>
+                                    <div className="operation-group">
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="radio"
+                                                        name="operation"
+                                                        value={this.state.operation}
+                                                        onChange={(e) => this.updateInput(e)}
+                                                        id="addition"
+                                                        value="1" required />
+                                                    <label className="form-check-label" htmlFor="addition">
+                                                        Addition (+)
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="radio"
+                                                        name="operation"
+                                                        value={this.state.operation}
+                                                        onChange={(e) => this.updateInput(e)} required
+                                                        id="subtraction"
+                                                        value="2" />
+                                                    <label className="form-check-label" htmlFor="subtraction">
+                                                        Subtraction (-)
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="radio"
+                                                        name="operation"
+                                                        value={this.state.operation}
+                                                        onChange={(e) => this.updateInput(e)} required
+                                                        id="multiplication"
+                                                        value="3" />
+                                                    <label className="form-check-label" htmlFor="multiplication">
+                                                        Multiplication (×)
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="radio"
+                                                        name="operation"
+                                                        value={this.state.operation}
+                                                        onChange={(e) => this.updateInput(e)}
+                                                        id="division" required
+                                                        value="4" />
+                                                    <label className="form-check-label" htmlFor="division">
+                                                        Division (÷)
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Submit Button */}
+                                <div className="d-grid">
+                                    <button type="submit" className="btn btn-primary btn-submit">
+                                        Calculate
+                                    </button>
+                                </div>
+                                <div className='text-center text-primary display-4'>
+                                    {this.state.answer}
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        );
+        )
     }
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BSE />)
+root.render(<Calculator />)
